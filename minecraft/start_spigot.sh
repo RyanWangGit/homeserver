1f26a2ac97f24dc6b096c10ad41585adef60d993
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-05-08 19:48:47 -0400

    Add some notes.

--- minecraft/start_spigot.sh
+++ minecraft/start_spigot.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# handles EULA
 if [ ! -z $EULA ]; then 
     echo "eula=$EULA" > /minecraft/eula.txt
 fi
 
+# create world folder if not present
 mkdir -p /config/world
 
+# start the spigot server
 java -Xmx2048M -Xms2048M -jar /minecraft/spigot-1.14.jar nogui \
     -c /config/server.properties \
     -b /config/bukkit.yml \
