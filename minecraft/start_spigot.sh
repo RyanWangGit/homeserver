003188a4c8e8465938f6278d96099a88d6ce2521
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-05-09 23:27:02 -0400

    Remove old plugins and create symlinks for the unsupported files of spigot.

--- minecraft/start_spigot.sh
+++ minecraft/start_spigot.sh
@@ -5,8 +5,25 @@ if [ ! -z $EULA ]; then
     echo "eula=$EULA" > /minecraft/eula.txt
 fi
 
+# copy the compiled plugins to config folder
+mkdir -p /config/plugins
+
+# remove older plugins
+rm -f /config/plugins/EssentialsX-*.jar
+rm -f /config/plugins/mcMMO*.jar
+rm -f /config/plugins/worldedit-bukkit-*.jar
+
+# move compiled plugins
+mv /plugins/*.jar /config/plugins/
+
 # create world folder if not present
 mkdir -p /config/world
+
+# symlinks the config files that cannot be specified from command line to spigot
+ln -s /config/ops.json /minecraft/ops.json
+ln -s /config/permissions.yml /minecraft/permissions.yml
+ln -s /config/whitelist.json /minecraft/whitelist.json
+ln -s /config/wepif.yml /minecraft/wepif.yml
 
 # start the spigot server
 java -Xmx2048M -Xms2048M -jar /minecraft/spigot-1.14.jar nogui \
@@ -14,4 +31,5 @@ java -Xmx2048M -Xms2048M -jar /minecraft/spigot-1.14.jar nogui \
     -b /config/bukkit.yml \
     -S /config/spigot.yml \
     -C /config/commands.yml \
-    -W /config/world
+    -W /config/world \
+    -P /config/plugins
