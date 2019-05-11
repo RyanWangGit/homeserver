b725ac50a2ea1e7007f7837d0e00aba9b88a93d1
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-05-11 00:45:34 -0400

    Xmx and Xms was accidentally swapped.

--- minecraft/start_spigot.sh
+++ minecraft/start_spigot.sh
@@ -28,7 +28,7 @@ ln -s /config/whitelist.json /minecraft/whitelist.json
 ln -s /config/wepif.yml /minecraft/wepif.yml
 
 # start the spigot server
-java -Xmx512M -Xms2048M -jar /minecraft/spigot-1.14.jar nogui \
+java -Xmx2048M -Xms512M -jar /minecraft/spigot-1.14.jar nogui \
     --noconsole \
     -c /config/server.properties \
     -b /config/bukkit.yml \
