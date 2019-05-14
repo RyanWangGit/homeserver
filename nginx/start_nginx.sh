0888df01d2530f4f7bf15d3af7f45d5dfb6d388e
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-05-13 23:06:59 -0400

    Initial commit of nginx.

---
+++ nginx/start_nginx.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# create symlink if external config file exists
+if [ -d "/config" ]; then
+    if [ -f "/config/nginx.conf" ]; then 
+        rm -f /etc/nginx/nginx.conf
+        ln -s /config/nginx.conf /etc/nginx/nginx.conf
+    else
+        cp /etc/nginx/nginx.conf /config/nginx.conf
+    fi
+fi
+
+# start nginx
+nginx -g daemon off;
