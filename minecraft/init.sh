77098dc06d619483afbd86d4d2887a11dcd09342
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-07-23 20:23:07 -0400

    Move normal user creation into init.sh script.

---
+++ minecraft/init.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# add a non-priviledged user
+groupadd -f -g {$PGID:-1000} abc
+useradd -g {$PGID:-1000} -M -u {$PUID:-1000} -s /bin/false abc
+
+# change the permissions of the files we will use
+mkdir -p /config
+chown abc:abc /config
+chown -R abc:abc /minecraft
+chown -R abc:abc /plugins
+chown abc:abc /start.sh
+
+setpriv --reuid=abc --regid=abc --clear-groups /start.sh
